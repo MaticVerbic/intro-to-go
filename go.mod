584e5035cf9e42fb35a10283c2a59e7f8eb5b709
Matic Verbič <matic.verbic@gymshark.com>
2025-03-18 21:18:31 +0100

    added todos endpoint

---
+++ go.mod
@@ -0,0 +1,13 @@
+module intro-to-go
+
+go 1.22.1
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ internal/handler/handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"encoding/json"
+	"intro-to-go/internal/model"
+	"net/http"
+)
+
+type Handler struct {
+	todos map[int]string
+}
+
+func (h *Handler) GetTodos(writer http.ResponseWriter, request *http.Request) {
+	h.todos = map[int]string{
+		1: "zbud se",
+		2: "pejt srat",
+	}
+
+	response := &model.TodosResponse{}
+	// naloga 1: if stavek, ki zapiše status code 404 Not Found,
+	// in message "no todos in memory", če v h.todos ni ničear
+	if len(h.todos) == 0 {
+		response.Message = "no todos in memory"
+		writer.WriteHeader(http.StatusNotFound)
+	}
+	// naloga 2: vrnemo vse elemented h.todos
+	response.Todos = h.todos
+	b, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte(err.Error()))
+		return
+	}
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(b)
+}
